af8c72eab2214a112fa67aacdd03d64030010378
aljoh <reachingalfred@gmail.com>
2017-11-30 20:41:04 +0100

    user unit tests in progress

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,25 @@ RSpec.describe User, type: :model do
       expect(FactoryBot.create(:user)).to be_valid
     end
   end
+
+  describe 'mailbox' do
+    before do
+      @sender = User.create(name: "Alfred", email: "whatever@whatever.com", password: "whatever")
+      @recipient = User.create(name: "Alfrod", email: "whatever@whatover.com", password: "whatover")
+      @sender.send_message(@recipient, 'message', 'subject')
+    end
+
+    it 'mailbox is created' do
+      #binding.pry
+      expect(@sender.mailbox).to be_truthy
+    end
+
+    it 'recipient has gotten a message' do
+      expect(@recipient.mailbox.inbox.count).to eq 1
+    end
+
+    it 'message has a subject' do
+      expect(@recipient.mailbox.inbox.last.subject).to eq 'subject'
+    end
+  end
 end
